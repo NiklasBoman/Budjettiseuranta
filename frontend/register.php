bbe6ec4aeaf113355f9529ca0a777d0c62295132
NiklasMonkkonen <niklas.monkkonen7@gmail.com>
2025-12-03 13:31:04 +0200

    tehty register

--- frontend/register.php
+++ frontend/register.php
@@ -1,1 +1,46 @@
+<!doctype html>
+<html>
+<head>
+<title>Kirjaudu sisään</title>
+<link rel="stylesheet" href="logintyyli.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="../frontend/assets/style.css">
+</head>
+<body>
+
+<div class="container">
+    <!-- Kirjautumislomake, joka lähettää tiedot login.php:lle POST-metodilla -->
+    <div class="vasen-paneeli">
+    <h2>Jatka rekisteröitymällä!</h2>
+    <p>Luo tili ja koe morderni budjetin seuranta</p>
+    </div>
+    <div class="oikea-paneeli">
+ <form action="login.php" method="post">
+        <!-- Lomakkeen otsikko -->
+<h2>Rekisteröityminen</h2>
+    <br>
+    
+    <label>Nimi</label>
+    <input type="text" name="nimi" id="nimi" placeholder="Etunimi Sukunimi" required>
+    <br>
+    <label>Sähköposti</label>
+    <input type="email" name="gmail" id="gmail" placeholder="nimi@example.com" required>
+    <br>
+    <label>Salasana</label>
+    <input type="password" name="salasana" id="salasana" placeholder="salasana" required>
+<div class="login-container">
+    <a href="login.php" class="login-link">Kirjautumaan</a>
+</div>
+<br>
+    <button type="submit">Rekisteröidy</button>
+</form>
+    </form>
+    </div>
+</div>
+</body>
+</html>
 
