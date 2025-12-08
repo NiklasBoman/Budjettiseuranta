c71db10d109f60b56f233f121ea8b9d522b57436
NiklasMonkkonen <niklas.monkkonen7@gmail.com>
2025-12-08 10:43:30 +0200

    header

--- frontend/includes/header.php
+++ frontend/includes/header.php
@@ -1,5 +1,5 @@
 <div class="header">
-    <img src="../frontend/assets/jolly-alarm.gif" alt="logo" class="logo">
+    <p>logo</p>
     <?php
     // Kirjaudu sisään/ulos painike riippuen siitä, onko käyttäjä kirjautunut vai ei
     session_start();
